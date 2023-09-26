b890123a515d5aad55bb35c16e8540204794ec22
Shabeer <shabeeralavi001@gmail.com>
2023-09-26 21:09:22 +0530

    edge extension for new tab is working

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const clickMeButton = document.getElementById("clickMe");
+    clickMeButton.addEventListener("click", function () {
+      alert("Hello, Edge!");
+    });
+  });
+  
